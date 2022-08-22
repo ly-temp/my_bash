dfde0e3558605b726713ee99a1f53da44c7d6ab3
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-08-22 18:11:13 +0800

    Delete fileio.sh

--- fileio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#$filename
-	curl -s -X 'POST' \
-	  'https://file.io/' \
-	  -H 'accept: application/json' \
-	  -H 'Content-Type: multipart/form-data' \
-	  -F "file=@$1" \
-	| grep -Eo '"link":"[^"]+"' | awk -F '"' '{print $(NF-1)}'
