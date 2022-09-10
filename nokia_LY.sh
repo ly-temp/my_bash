89d9c4995e55d4a62c900ba6c6729896443fa743
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-09-10 15:39:16 +0800

    Add files via upload

--- nokia_LY.sh
+++ nokia_LY.sh
@@ -10,14 +10,14 @@ fi
 
 echo "type: $type"
 
-awk_bc(){
-	awk '{print $1 + $2}'
-}
-
 diff_db(){
-	current_db=($(ffmpeg -i "$file" -filter:a volumedetect -f null /dev/null 2>&1 | grep "mean_volume:" | grep -o ":.*" | cut -d' ' -f2))
+	current_db=($(ffmpeg -i "$file" -filter:a volumedetect -f null /dev/null 2>&1 | grep "mean_volume:" | grep -o ":.*" | cut -d' ' -f2 | tee -a log.txt))
+	#bc
 	#diff=$(echo "$target_db - $current_db" | bc)
-	diff=$(awk_bc <<< "$target_db -$current_db")
+	#awk
+	
+	diff=$(awk '{print $1 - $2}' <<< "$target_db $current_db")
+	
 	echo "$diff"
 }
 
@@ -32,7 +32,7 @@ while
   fi
 
   #value=$(echo "$value + $diff" | bc)
-  value=$(awk_bc <<< "$value $diff")
+  value=$(awk '{print $1 + $2}' <<< "$value $diff")
   str_value="$value""dB"
   out_f="${1%.*}[$str_value]$suffix"
   echo "parameter: $str_value"
--- to_db_LY.sh
+++ to_db_LY.sh
@@ -8,14 +8,10 @@ else
 	suffix=$3
 fi
 
-awk_bc(){
-	awk '{print $1 + $2}'
-}
-
 diff_db(){
 	current_db=($(ffmpeg -i "$file" -filter:a volumedetect -f null /dev/null 2>&1 | grep "mean_volume:" | grep -o ":.*" | cut -d' ' -f2))
 	#diff=$(echo "$target_db - $current_db" | bc)
-	diff=$(awk_bc <<< "$target_db -$current_db")
+	diff=$(awk '{print $1 - $2}' <<< "$target_db $current_db")
 	echo "$diff"
 }
 
@@ -30,7 +26,7 @@ while
   fi
 
   #value=$(echo "$value + $diff" | bc)
-  value=$(awk_bc <<< "$value $diff")  
+  value=$(awk '{print $1 + $2}' <<< "$value $diff")  
   str_value="$value""dB"
   out_f="${1%.*}[$str_value]$suffix"
   echo "parameter: $str_value"
