ad54e6d1b0c6552adb6f8b540627c80278bef669
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-11-23 08:10:00 +0800

    Add files via upload

--- blackboard_dl.sh
+++ blackboard_dl.sh
@@ -4,7 +4,6 @@ extract_cookie_LY.sh "$2"
 
 html=$(realpath "$1")
 list=$(scan_html_LY.sh "file://$html" | grep pid | sed "s|file://|https://blackboard.cuhk.edu.hk|g" )
-echo "$cookie"
 for url in $list;do
 	curl -L "$url" -H @"$2" -O -J
 done
