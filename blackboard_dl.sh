9b7b32748bb4e1b14355b7fb44738ea68cf52176
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-11-23 08:45:54 +0800

    Add files via upload

--- blackboard_dl.sh
+++ blackboard_dl.sh
@@ -6,10 +6,11 @@ function urldecode(){
 
 extract_cookie_LY.sh "$2"
 
+mkdir -p dl
 html=$(realpath "$1")
 list=$(scan_html_LY.sh "file://$html" | grep pid | sed "s|file://|https://blackboard.cuhk.edu.hk|g" )
 for url in $list;do
-	filename=$(curl -sLI "$url" -H @"$2" | grep "content-disposition:" | awk -F\' '{print $NF}'| urldecode)
+	filename=$(curl -sLI "$url" -H @"$2" | grep "content-disposition:" | tr -d '\r','\n' | awk -F\' '{print $NF}'| urldecode )
 	#curl -sL "$url" -H @"$2" -OJ -D- | grep content-disposition | awk -F\' '{print $NF}'
-	curl -sL "$url" -H @"$2" -o "$filename"
+	curl -sL "$url" -H @"$2" -o "dl/$filename"
 done
