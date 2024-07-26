e6ab8cc1a6c449bac5ec484d76c6b860d8e4ec6a
ly-temp <100673133+ly-temp@users.noreply.github.com>
2024-07-26 08:31:18 +0800

    Update colab_starter.sh

--- colab_starter.sh
+++ colab_starter.sh
@@ -4,5 +4,5 @@ cd /tmp
 wget -q https://github.com/ly-temp/my_bash/archive/refs/heads/main.zip
 mkdir my_bash
 7z e main.zip -omy_bash -aoa
-chmod +x my_bash/* && sudo mv my_bash/*.sh /bin
+chmod +x my_bash/* && sudo mv my_bash/*.sh /usr/local/bin
 rm -fr my_bash
